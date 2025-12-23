daf9b99125eedb28bae0d57aa3ab45170a13149e
Nicol-Parra <231162@unamba.edu.pe>
2025-12-22 19:21:43 -0500

    cambio 1

--- portafolio-personal/js/header.js
+++ portafolio-personal/js/header.js
@@ -1,1 +1,16 @@
+// Header Scroll Effect
+let lastScrollTop = 0;
+
+window.addEventListener('scroll', () => {
+    const header = document.getElementById('header');
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    
+    if (scrollTop > 50) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+    
+    lastScrollTop = scrollTop;
+});
 
